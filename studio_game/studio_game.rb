2f07ba2a9d25b5b28d31ea2cfd8c2788cc0393f1
cspmosh <cs.pmosh@gmail.com>
2020-09-24 20:50:19 -0500

    exercise code for strings and numbers lesson

--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -1,7 +1,12 @@
-greeting = "Welcome!"
+name1 = "larry"
+health1 = 60
 
-3.times do
-  puts greeting.upcase
-end
+name2 = "curly"
+health2 = 60
 
-puts Time.new
+name3 = "moe"
+health3 = 60
+
+puts "#{name1}'s health is #{health1}"
+puts "Players:\n\t#{name1}\n\t#{name2}\n\t#{name3}"
+
