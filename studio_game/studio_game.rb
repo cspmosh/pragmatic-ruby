5c1081adb5c8a4ee226a3ed41acb53e2d1371d50
cspmosh <cs.pmosh@gmail.com>
2020-09-24 21:25:23 -0500

    lesson 2 exercise code with objects and methods

--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -1,12 +1,20 @@
+current_time = Time.new
+formatted_time = current_time.strftime("%A %m/%d/%Y at %I:%M%p")
+
 name1 = "larry"
 health1 = 60
 
 name2 = "curly"
-health2 = 60
+health2 = 125
 
 name3 = "moe"
-health3 = 60
+health3 = 100
 
-puts "#{name1}'s health is #{health1}"
-puts "Players:\n\t#{name1}\n\t#{name2}\n\t#{name3}"
+name4 = "shemp"
+health4 = 90
 
+puts "The game started on #{formatted_time}"
+puts "#{name1.capitalize} has a health of #{health1}."
+puts "#{name2.upcase} has a health of #{health2}."
+puts "#{name3.capitalize} has health of #{health3}.".center(50, '*')
+puts "#{name4.capitalize.ljust(30, '.')} #{health4} health"
